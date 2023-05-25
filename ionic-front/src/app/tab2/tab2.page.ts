0826bc0d52e9654a097ffae350c67b1d33426e5d
fjluna <fjlunapalma@gmail.com>
2023-05-25 09:51:46 +0200

    Filtrado funcional

--- ionic-front/src/app/tab2/tab2.page.ts
+++ ionic-front/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public dataService: DataService, private router: Router) {}
+  products: any;
+  filterProducts: any;
 
+  ngOnInit() {
+    this.dataService.getProducts().then((products: any) => {
+      this.products = products;
+      console.log(this.products);
+    }
+    )
+  }
+
+  async cerrarSesion() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+  
+  productsFilter(event: any) {
+    const textoBusqueda = event.detail.value;
+
+    if (textoBusqueda && textoBusqueda.trim() !== '') {
+      this.filterProducts = this.products.filter((product: { nombre: string; }) =>
+        product.nombre.toLowerCase().includes(textoBusqueda.toLowerCase())
+      );
+      console.log(this.filterProducts);
+    } else {
+      this.filterProducts = [];
+    }
+  }
 }
+
+
